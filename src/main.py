dfd3f975c49c9c3e40dfec9cd4446c266e86cb76
Vladyslav <siriusbrightestone@gmail.com>
2024-06-21 15:47:30 +0200

    Update main.py

--- src/main.py
+++ src/main.py
@@ -4,8 +4,6 @@ from diet_helper import MacroManager, FoodLister, Foods, Meal, copy
 
 def main_function():
     test = Foods(db_type='mongo')
-    print(test)
-    # test.add_food('chicken breast', 31, 4, 0)
     copy.copy_foods_to_local_json(test.foods, 'json/local_copy.json')
     
 if __name__ == "__main__":
