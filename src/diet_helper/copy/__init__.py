ad31e0a54e6277be45069529f5d4aaaf84fa05a5
RunnyWater <siriusbrightestone@gmail.com>
2024-06-24 14:28:21 +0200

    added new functionality - new copy function, that will copy your meals, will be described 'how to' on launch later

--- src/diet_helper/copy/__init__.py
+++ src/diet_helper/copy/__init__.py
@@ -12,3 +12,15 @@ def copy_foods_to_local_json(foods:dict, json_file_path='json/foods.json'):
         data[food_name] = foods[food_name].get_info_for_json()
     with open(json_file_path, 'w') as f:
         json.dump(data, f, indent=4)
+
+def copy_meals_to_local_json(meals:dict, json_file_path='json/meals.json'):
+    if not os.path.exists(json_file_path):
+        if input(f"{json_file_path} not found, do you want to create it? (y/n)") == 'y': 
+            os.makedirs(os.path.dirname(json_file_path), exist_ok=True)
+        else: 
+            return exit('Please create json file')
+    data = {}
+    for meal_id in meals.keys():
+        data[meal_id] = meals[meal_id].get_info_for_json()
+    with open(json_file_path, 'w') as f:
+        json.dump(data, f, indent=4)
