390f55809e27fba30ccf018964bf52796af092ac
Vladyslav Krasynskyi <siriusbrightestone@gmail.com>
2024-07-18 17:59:49 +0200

    started working on Voice Asistant

--- src/main.py
+++ src/main.py
@@ -1,16 +1,10 @@
 from diet_helper import Days
-
+from voice_assistant import VoiceAssistant
 
 
 def main_function():
     test = Days(db_type='mongo')
-    # print(test.days)
-    for day in test.days.values():
-        print(day)
 
-    # print(test.get_meal_by_id(0).get_macros())
-    # test.get_days_mongo()
 
 if __name__ == "__main__":
     main_function()
-
