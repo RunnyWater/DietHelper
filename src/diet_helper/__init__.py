f6dc78ae8248f0a36fd67df165790643184ee2f0
Vladyslav Krasynskyi <siriusbrightestone@gmail.com>
2024-08-19 23:38:35 +0200

    starting to work on the main menu functionallity

--- src/diet_helper/__init__.py
+++ src/diet_helper/__init__.py
@@ -5,12 +5,12 @@ from .days import Days
 import inspect
 
 class TextMenu:
-    def handle_user_input(self, options:dict):
+    def handle_user_input(self, options:dict) -> None:
         while True:
             print("\nPlease select an option:")
-            for key, value in self.options.items():
+            for key, value in options.items():
                 print(f"{key}: {value}")
-            choice = input("Enter the number of your choice: ")
+            choice:str = input("Enter the number of your choice: ")
 
             if choice.lower() == 'q':
                 print("Exiting...")
@@ -20,26 +20,26 @@ class TextMenu:
                 # Convert choice to an integer if it's a valid option
                 if choice.isdigit():
                     choice = int(choice)
-                func_name = self.options[choice]
+                func_name:str = options[choice]
                 
                 # Get the function reference
-                func = getattr(self.days, func_name)
+                func = getattr(self.database, func_name)
                 
                 # Get the function signature to see if it requires arguments
-                signature = inspect.signature(func)
-                params = signature.parameters
+                signature:inspect.Signature = inspect.signature(func)
+                params:inspect.Parameter = signature.parameters
 
                 # Check if the function requires arguments
                 if params:
                     # Ask user to provide arguments
-                    args = []
-                    kwargs = {}
+                    args:list = []
+                    kwargs:dict = {}
                     for name, param in params.items():
                         if param.default == inspect.Parameter.empty:
-                            value = input(f"Enter the value for '{name}': ")
+                            value:str = input(f"Enter the value for '{name}': ")
                             args.append(value)
                         else:
-                            value = input(f"'{name}' (Leave blank to use '{param.default}' value): ")
+                            value:str = input(f"'{name}' (Leave blank to use '{param.default}' value): ")
                             if value:
                                 kwargs[name] = value
 
@@ -67,7 +67,7 @@ class DaysMenu(TextMenu):
         
         # TODO: add possibility to use full options 
         # self.options = {1 :'add_meal', 2 :'add_day', 3 :'get_total_macros_by_date', 4 :'get_meal_macros_by_date', 5 :'get_all_meals_macros_by_date', 6 :'exit'}
-        self.options = {
+        self.options:dict = {
             1: 'add_meal',
             2: 'add_day',
             3: 'get_food',
@@ -76,9 +76,8 @@ class DaysMenu(TextMenu):
             6: 'get_all_meals_macros_by_date',
             'q': 'return'
         }
-        self.days = Days()
-
-        self.text_menu = self.handle_user_input(self.options)
+        self.database:Days = Days()
+        self.handle_user_input(self.options)
 
 class FoodsMenu(TextMenu):
 
@@ -86,7 +85,7 @@ class FoodsMenu(TextMenu):
         
         # TODO: add possibility to use full options 
         # self.options = {1 :'add_food', 2 :'add_variant', 3 :'delete_food', 4 :'delete_variant', 5 :'get_foods', 'q' :'exit'}
-        self.options = {
+        self.options:dict = {
             1 :'add_food', 
             2 :'add_variant', 
             3 :'delete_food',
@@ -94,6 +93,83 @@ class FoodsMenu(TextMenu):
             5 :'get_foods',
             6 :'get_food',
             'q' :'return'}
-        self.foods = Foods()
+        self.database:Foods= Foods()
 
-        self.text_menu = self.handle_user_input(self.options)
+        self.handle_user_input(self.options)
+
+class MainMenu(TextMenu):
+    def __init__(self) -> None:
+        self.options = {1: 'days', 2: 'foods', 3: 'change_database_type', 'q': 'exit'}
+        self.days_menu = DaysMenu()
+        self.foods_menu = FoodsMenu()
+        self.handle_user_input()
+        all_members = inspect.getmembers(self)
+
+        # Filter to include only functions
+        functions = [member[0] for member in all_members if inspect.isfunction(member[1])]
+
+    # override of the parent function due to options linking to the classes itself and that requires another handling method
+    def handle_user_input(self) -> None:
+        while True:
+            print("\nPlease select an option:")
+            for key, value in self.options.items():
+                print(f"{key}: {value}")
+            choice:str = input("Enter the number of your choice: ")
+
+            if choice.lower() == 'q':
+                print("Exiting...")
+                break
+
+            try:
+                # Convert choice to an integer if it's a valid option
+                if choice.isdigit():
+                    choice = int(choice)
+                func_name:str = self.options[choice]
+                
+                func = getattr(self, func_name)
+
+                # Get the function signature to see if it requires arguments
+                signature:inspect.Signature = inspect.signature(func)
+                params:inspect.Parameter = signature.parameters
+
+                # Check if the function requires arguments
+                if params:
+                    # Ask user to provide arguments
+                    args:list = []
+                    kwargs:dict = {}
+                    for name, param in params.items(): 
+                        if param.default == inspect.Parameter.empty:
+                            value:str = input(f"Enter the value for '{name}': ")
+                            args.append(value)
+                        else:
+                            value:str = input(f"'{name}' (Leave blank to use '{param.default}' value): ")
+                            if value:
+                                kwargs[name] = value
+
+                    # Call the function with the collected arguments
+                    result = func(*args, **kwargs)
+                else:
+                    # Call the function without arguments
+                    result = func()
+                    
+                print("Function executed successfully. Result:", result)
+                
+                
+            except KeyError:
+                print("Invalid option. Please choose a valid number.")
+            except AttributeError:
+                print("Function not found. Please choose a valid option.")
+            except TypeError as e:
+                print(f"Argument error: {e}")
+            except Exception as e:
+                print(f"An error occurred: {e}")
+
+
+    def change_database_type(self):
+        pass
+
+    def days(self):
+        pass
+
+    def foods(self):
+        pass
--- src/main.py
+++ src/main.py
@@ -1,10 +1,9 @@
-from diet_helper import Days, DaysMenu, FoodsMenu
+from diet_helper import MainMenu
 # from voice_assistant import VoiceAssistant
 
 
 def main_function():
-    # test = Days(db_type='json')
-    test = DaysMenu() if input('d/f?') == 'd' else FoodsMenu()
+    test = MainMenu()
     
 
 
